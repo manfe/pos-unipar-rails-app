100e8e6d0e12c8d1c7af087a974ee3485fbf65cf
Mauricio N. Ferreira <contato@ferreiramauricio.com>
2022-10-15 09:46:53 -0300

    Estrututa base do projeto next.js para o front

---
+++ front-next/README.md
@@ -0,0 +1,14 @@
+## RODAR O SERVIDOR
+
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+# USUÁRIOS WSL2 QUE ENFRETAM PROBLEMA COM O FAST REFRESH
+
+```bash
+echo "WATCHPACK_POLLING=true" > .env.local
+```
---
+++ front-next/pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <h1>Primeiro Projeto em Next.js</h1>
+  )
+}
