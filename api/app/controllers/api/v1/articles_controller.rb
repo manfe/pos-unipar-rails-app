cfde32dcd5305bebdf47364710daf2bc80621006
Mauricio N. Ferreira <contato@ferreiramauricio.com>
2022-10-15 16:56:43 -0300

    Commentado linhas para não necessitar de auth por enquanto

--- api/app/controllers/api/v1/articles_controller.rb
+++ api/app/controllers/api/v1/articles_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::ArticlesController < ApplicationController
 
   # GET /articles
   def index
-    sleep 2
     @articles = Article.all
 
     render json: @articles, include: [:category, :author]
--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -2,15 +2,15 @@ class ApplicationController < ActionController::API
 
   include JsonWebToken
 
-  # before_action :authenticate_request
+  before_action :authenticate_request
 
   private
-    def authenticate_request
-      authorization_string = request.headers["Authorization"]
-      token = authorization_string.split(" ").last if authorization_string
+  
+  def authenticate_request
+    # authorization_string = request.headers["Authorization"]
+    # token = authorization_string.split(" ").last if authorization_string
 
-      decoded = jwt_decode(token)
-      @current_user = User.find(decoded[:user_id])
-    end
-
+    # decoded = jwt_decode(token)
+    # @current_user = User.find(decoded[:user_id])
+  end
 end
